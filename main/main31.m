15d86ff6fc1c24c0c6a3487c26c1e7031028f14b
giammyjeck <gianmarco.foni@studenti.polito.it>
2026-01-29 04:01:47 +0100

    tunind trunnc ma mica tanto

--- main/main31.m
+++ main/main31.m
@@ -6,16 +6,16 @@ rng(seed);
 addpath(genpath(pwd))
 
 % Defining the problem
-[f, gradf, hessf, xbar_gen] = problem_broyden31();
+[f, gradf, hessf, xbar_gen] = problem14();
 
 % Parameters definition
-dimensions = [2, 10^3]%, 10^4, 10^5]; 
-kmax = 50;
+dimensions = [2, 10^3, 10^4 ];%, 10^5]; 
+kmax = 200;
 tolgrad = 1e-6;
-c1 = 1e-3;      % Standard Armijo parameter
-rho = 0.3;      % Backtracking contraction factor
-btmax = 5;
-max_cg = 500;   % Max inner iterations for the conjugate gradient solving method in the truncated one
+c1 = 1e-4;      % Standard Armijo parameter
+rho = 0.8;      % Backtracking contraction factor
+btmax = 20;
+max_cg = 1000;   % Max inner iterations for the conjugate gradient solving method in the truncated one
 beta = 1e-2;
 
 % Data structures to store results 
